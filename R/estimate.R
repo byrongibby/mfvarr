df10aa5ebad08579a4e15795d76b10b5b39df726
Byron Botha <byron.botha@resbank.co.za>
2019-02-04 12:01:14 +0200

    Typo corrected.

--- R/estimate.R
+++ R/estimate.R
@@ -68,7 +68,7 @@ estimate <- function(m, reps, burn, seed=set.seed(runif(1,0,1e6)))
     if(i > burn) {
       post$Y[i - burn,] <- as.vector(Z.draw[,(M+1):(K+1)])
       post$Z[i - burn,] <- as.vector(Z.draw[,])
-      post$B[i - burn,] <- b.draw
+      post$A[i - burn,] <- b.draw
       post$S[i - burn,] <- as.vector(S.draw)
     }
   }
